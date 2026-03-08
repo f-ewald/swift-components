911dd299c7eaeccd2b334ee30d03a7be4ff4037e
Friedrich Ewald <fewald@linkedin.com>
2026-03-08 12:45:06 -0700

    Add support for subview in Logo view

--- Sources/SharedComponents/LogoVersionView.swift
+++ Sources/SharedComponents/LogoVersionView.swift
@@ -7,34 +7,49 @@
 
 import SwiftUI
 
-public struct LogoVersionView: View {
-    let logo: String
+public struct LogoVersionView<Content: View>: View {
+    let logo: String?
+    let logoView: Content?
     let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
     let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown"
     
-    public init(logo: String) {
+    public init(logo: String) where Content == EmptyView {
         self.logo = logo
+        self.logoView = nil
+    }
+    
+    public init(@ViewBuilder logoView: () -> Content) {
+        self.logo = nil
+        self.logoView = logoView()
     }
     
     public var body: some View {
         VStack {
             HStack {
                 Spacer()
-                Image(logo)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 40, height: 40)
+                if let logoView {
+                    logoView
+                } else if let logo {
+                    Image(logo)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                }
                 Spacer()
             }
             HStack {
                 Spacer()
-                Text("Version \(version) (\(build))").font(.footnote).foregroundStyle(.tertiary)
+                Text("Version \(version) (\(build))").font(.footnote).foregroundStyle(.secondary)
                 Spacer()
             }
         }
     }
 }
 
-#Preview {
+#Preview("Image Resource") {
     LogoVersionView(logo: "Logo")
+}
+
+#Preview("Subview") {
+    LogoVersionView(logoView: { Text("Hello") })
 }
